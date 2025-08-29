e765d83814eccba78d1b647e7ae735eda2693bd9
PranavKrSingh <pranavkumarsingh32@gmail.com>
2025-08-29 15:26:53 +0530

    Fix deployment port for Render

--- app.py
+++ app.py
@@ -62,4 +62,6 @@ def result():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=7384)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
+
